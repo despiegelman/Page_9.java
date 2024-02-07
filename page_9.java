d979f3b0f80210dcc95ae5ebe81ca4c2e0ef4e69
dspiegelman <mtvesuvius67@gmail.com>
2024-02-07 14:38:42 -0500

    Created page_9.java

---
+++ page_9.java
@@ -0,0 +1,16 @@
+/**
+ * Project: Page 9
+ * Programmer: Delia Spiegelman
+ * Date: 2/7/2024
+ * Program Name: page_9.java
+ */
+public class page_9 
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Hi"); //mesage to user
+        System.out.println("LabOneA"); //assignement
+    }
+}
+/* if you want to write a detailed comments, and it wraps around the 
+screen, use this block style of commenting.*/
